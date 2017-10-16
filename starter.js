69e8d632920ff411725b96d2553a1c87b12a5701
Sara Victoria Lopez <slopez12@hawk.iit.edu>
2017-10-16 17:53:59 -0500

    keywords in node

--- starter.js
+++ starter.js
@@ -6,11 +6,34 @@ const user;
 user = { name: "Sara", age: 22 };
 
 if(true) {
-  var dog = "Mr.wag"
+  var dog = "Mr.wag"; // dog os scoped to block
 }
 
-const colors = ['red', 'green', 'blue'];
+const colors = ['red', 'green', 'blue']; // colors is constant
 
-for (var j = 0; j < colors.length; i++) {
-  console.log(color);
+// j is scoped to block
+for (var j = 0; j < colors.length; j++) {
+  console.log(colors[j]);
 }
+
+// secret is scopped to the function; var world exhibit same behavior:
+function secretKeeper() {
+  var secret = "Tell nobody!";
+}
+
+// topsecret is not secret at all; without const, let, or var, 'topsecret' appears in the global scope:
+function theGossiper() {
+  topsecret = "I promoise not to tell nobody";
+}
+
+const teacher = "Karl";
+const course = "COM 333/533: API";
+
+console.log(teacher + " is the instructor for " + course);
+
+const sayTwelve = () => { return 12; }
+
+// ^ that's the short form for this:
+//const sayTwelve = function () {
+//  return 12;
+//}
