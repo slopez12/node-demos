fb54f7db2938185286e6b77243c443cdf19c4dbe
Sara Victoria Lopez <slopez12@hawk.iit.edu>
2017-10-16 17:34:27 -0500

    first commit

---
+++ starter.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// var user;
+const user;
+
+user = { name: "Sara", age: 22 };
+
+if(true) {
+  var dog = "Mr.wag"
+}
+
+const colors = ['red', 'green', 'blue'];
+
+for (var j = 0; j < colors.length; i++) {
+  console.log(color);
+}
